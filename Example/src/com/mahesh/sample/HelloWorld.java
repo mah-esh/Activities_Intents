ae03c8330ad462691e4d0233a0a57b6839a09de9
mah-esh <maheswara582s@gmail.com>
2016-12-14 17:07:53 +0530

    code for hello_world

---
+++ Example/src/com/mahesh/sample/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.mahesh.sample;
+
+
+import com.example.example.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorld extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.hello_world);
+
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+
+}
