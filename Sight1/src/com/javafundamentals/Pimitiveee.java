a4fa8d5c66cbfb1f51d7699984db24f1b6165143
mah-esh <maheswara582s@gmail.com>
2016-12-27 17:54:32 +0530

    explicit intent 3 binary files

---
+++ Sight1/src/com/javafundamentals/Pimitiveee.java
@@ -0,0 +1,10 @@
+package com.javafundamentals;
+
+public class Pimitiveee {
+public static void main(String[] args) {
+	char regularU = 'U';
+	char accentedU = '\u00DA';
+	System.out.println(regularU);
+	System.out.println(accentedU);
+}
+}
